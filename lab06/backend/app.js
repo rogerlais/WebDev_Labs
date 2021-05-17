f9ca15d3058728e90c15b3bc9cd153cb8ac95e09
rogerlais <rogerlais@gmail.com>
2021-05-16 22:15:54 -0300

    pré-upload to replit

--- lab06/backend/app.js
+++ lab06/backend/app.js
@@ -123,4 +123,12 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
+
+app._router.stack.forEach(function (r) {
+	if (r.route && r.route.path) {
+		console.log(r.route.path);
+	}
+});
+
+
 module.exports = app;
